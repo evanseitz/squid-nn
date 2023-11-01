6e5acf791eaa4bc54073509da5e028f37040d117
Evan Seitz <evan.e.seitz@gmail.com>
2023-11-01 09:10:03 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ html_context = {
     ###else:
         ###version_match = "v" + release
 
-html_theme = "pydata_sphinx_theme"
+html_theme = 'sphinx_rtd_theme' #"pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
