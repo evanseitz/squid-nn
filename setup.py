38118147bb17ee8330bb083fede34a13862b29c1
Evan Seitz <evan.e.seitz@gmail.com>
2024-02-15 13:44:34 -0500

    versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="squid-nn",
-    version="0.3.7",
+    version="0.3.8",
     author="Evan Seitz",
     author_email="evan.e.seitz@gmail.com",
     packages=find_packages(),
@@ -14,9 +14,9 @@ setup(
     ],
     python_requires=">=3.7.2",
     install_requires=[
-    	'numpy',
+    	'numpy==1.21.6',
 	'matplotlib>=3.2.0',
-	'pandas',
-        'tqdm',
+	'pandas==1.3.5',
+        'tqdm==4.66.1',
     ],
 )
